48da4163928a212308cb2691361e868ed9d65fc9
Simelie <xoraob@gmail.com>
2021-04-15 07:55:04 +0200

    Add functions playerSelection and singleRound

--- app.js
+++ app.js
@@ -4,5 +4,23 @@ function computerPlay(){
     return element[randomNumber];
 }
 
-const element = computerPlay();
-console.log(element);
+function playerSelection(){
+    let choix = '';
+    while(choix !== 'ROCK' && choix !== 'PAPER' && choix !== 'SCISSORS'){
+        choix = prompt("Rock, paper or scissors ?").toUpperCase();
+    }
+    return choix;
+}
+
+function singleRound(computerSelection, playerSelection){
+    //BUG A CORRIGER
+    if (computerSelection === 'ROCK' && playerSelection !== 'PAPER') return `You Lose! Rock beats ${playerSelection}`; 
+    if (computerSelection === 'PAPER' && playerSelection !== 'SCISSORS') return `You Lose! Paper beats ${playerSelection}`; 
+    if (computerSelection === 'SCISSORS' && playerSelection !== 'ROCK') return `You Lose! Scissors beats ${playerSelection}`; 
+    if (computerSelection === playerSelection) return `Equality!`;
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+}
+
+const computerElement = computerPlay();
+const playerElement = playerSelection();
+console.log(singleRound(computerElement, playerElement));
